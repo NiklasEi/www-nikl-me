c49b6001ff692807daef0f37cf64951ee392bc07
Niklas Eicker <git@nikl.me>
2020-10-24 15:41:47 +0200

    WIP: gatsby setup

---
+++ src/gatsby-api/config.ts
@@ -0,0 +1,15 @@
+export const siteMetadata = {
+  title: 'Nikl.me',
+  description: '',
+  keywords: 'nikl, gamedev',
+  siteUrl: 'https://www.nikl.me',
+  author: {
+    name: 'Niklas Eicker',
+    url: 'https://www.nikl.me',
+    email: 'hello@nikl.me'
+  }
+};
+
+export const plugins = [
+  'gatsby-plugin-typescript'
+];
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+const IndexPage: React.FC = () => {
+    return <p>Hi there</p>
+}
