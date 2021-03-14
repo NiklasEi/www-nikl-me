a40bd93baf171d16a3b03e073a4c567960aa934a
Niklas Eicker <git@nikl.me>
2021-03-14 22:45:44 +0100

    Move custom 404 page to a component

---
+++ src/components/PageNotFound/PageNotFound.styles.ts
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+export const StyledPageNotFound = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const StyledPageNotFoundImg = styled.img`
+  max-width: 300px;
+`;
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,27 +1,9 @@
 import React from 'react';
 import withDefaultLayout from '../layouts/default';
-import styled from '@emotion/styled';
+import PageNotFoundComponent from "../components/PageNotFound/PageNotFound";
 
 const PageNotFound: React.FC = () => {
-  return (
-    <StyledPageNotFound>
-      <h1>404</h1>
-      <StyledPageNotFoundImg src="/assets/images/ruffyAvatar.jpg" alt="The future pirate king" />
-
-      <p>I cannot find the page you are looking for...</p>
-      <p>But you found the future pirate king! Congrats ;)</p>
-    </StyledPageNotFound>
-  );
+  return <PageNotFoundComponent/>;
 };
 
 export default withDefaultLayout(PageNotFound);
-
-const StyledPageNotFound = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const StyledPageNotFoundImg = styled.img`
-  max-width: 300px;
-`;
